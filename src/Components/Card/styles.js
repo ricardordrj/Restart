117d7c1ae30de8201b6cbe2668f00d4dec0a93ed
Ricardo <ricardordrj@gmail.com>
2022-06-13 16:46:12 -0300

    adding media query to gallery items

--- src/Components/Card/styles.js
+++ src/Components/Card/styles.js
@@ -15,10 +15,19 @@ export const CardLink = styled.a`
     width: 150px;
     height: 150px;
     transition: all 0.1s ease-in;
+    background-repeat: no-repeat;
+    background-position: center center;
+    @media (max-width: 800px) {
+      width: 220px;
+      height: 220px;
+    }
     ${({ bgImg }) =>
       css`
         background-image: url(${bgImg});
         background-size: 150px;
+        @media (max-width: 800px) {
+          width: 220px;
+        }
       `}
     &:hover {
       transform: scale(1.1);
